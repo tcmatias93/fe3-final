2d496cc5828851c5c823179eb097d695e05bd98a
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-11-28 10:04:13 -0600

    Se agrego funcionalidad de favoritos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,27 @@
 const Card = ({ user }) => {
   const { name, username } = user;
   const addFav = () => {
-    // Aqui iria la logica para agregar la Card en el localStorage
+    const existingFavs = JSON.parse(localStorage.getItem("doctor")) || [];
+    const isAlreadyFav = existingFavs.some((fav) => fav.id === user.id);
+
+    if (!isAlreadyFav) {
+      const updatedFavs = [...existingFavs, user];
+      localStorage.setItem("doctor", JSON.stringify(updatedFavs));
+
+      alert(`${name} agregado a favoritos!`);
+    } else {
+      alert(`${name} ya está en favoritos.`);
+    }
   };
 
   return (
     <div className="card">
-      {/* En cada card deberan mostrar en name - username y el id */}
-
       <img src="../../public/images/doctor.jpg" alt="Imagen de doctor" />
       <p> {name} </p>
       <p>{username}</p>
 
       {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
-      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
       <button onClick={addFav} className="favButton">
         Add fav
       </button>
--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Favs = () => {
+  const docFav = JSON.parse(localStorage.getItem("doctor")) || [];
 
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {docFav.map((data) => (
+          <Card key={data.id} user={data} />
+        ))}
       </div>
     </>
   );
