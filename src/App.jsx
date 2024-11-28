3925450c8d39cf8e760db4fe5177f064e06cf5e7
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-11-27 19:55:07 -0600

    Rutas agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/fav" element={<Favs />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
