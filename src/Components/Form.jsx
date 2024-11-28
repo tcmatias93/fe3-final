0235ba7e3211eb1605d98038430fb68187cd12df
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2024-11-28 08:49:27 -0600

    Se agrego navbar

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-
-
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
   return (
     <div>
       <form>
+        <input type="text" />
+        <input type="text" />
+        <button>Send</button>
       </form>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
-}
 
-export default Navbar
+      <Link to={"/"} className="link">
+        <img src="../../public/DH.ico" alt="" />
+      </Link>
+
+      <Link to={"/"} className="link">
+        Home
+      </Link>
+      <Link to={"/contact"} className="link">
+        Contact
+      </Link>
+      <Link to={"/fav"} className="link">
+        Fav
+      </Link>
+
+      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+      <button className="favButton">Change theme</button>
+    </nav>
+  );
+};
+
+export default Navbar;
